dd1efa21e665d763768167a030c2a2ec656a24ca
Ruman Gerst <ruman.gerst@uni-jena.de>
2017-03-30 23:19:41 +0200

    * Refactoring

--- server.R
+++ server.R
@@ -142,66 +142,14 @@ shinyServer(function(input, output, session) {
   downloadablePlot("conditions.plot",
                    plot.settings = pca.conditions.plot.generalsettings,
                    exprplot = function(plot.settings, format, filename) {
-                     
-                     validate(need(conditions(), "No conditions to plot!"),
-                              need(pca.conditions.plot.visuals(), "No condition visuals available!"))
-                     selected.conditions <- input$conditions.plot.sets
-                     
-                     validate(need(selected.conditions, "No conditions selected!"),
-                              need(length(selected.conditions) > 0, "No conditions selected!"),
-                              need(length(selected.conditions) <= 5, "Too many conditions selected!"))
-                     
-                     x <- lapply(selected.conditions, function(x) { 
-                       
-                       indices <- conditions()[[x]]
-                       return(rownames(conditions())[indices]) 
-                       
-                       })
-                     names(x) <- selected.conditions
-                     
-                     plot.settings <- setNA(plot.settings, PlotSettings(
-                       width = 640,
-                       height = 480,
-                       dpi = 96,
-                       title = "Conditions",
-                       subtitle = "Sub"
+                     return(saveCellConditionVennDiagramPlot(
+                       selected.conditions = input$conditions.plot.sets,
+                       conditions = conditions,
+                       pca.conditions.plot.visuals = pca.conditions.plot.visuals,
+                       plot.settings = plot.settings,
+                       format = format,
+                       filename = filename
                      ))
-                     
-                     width <- plot.settings@width
-                     height <- plot.settings@height
-                     dpi <- plot.settings@dpi
-                     title <- plot.settings@title
-                     subtitle <- plot.settings@subtitle
-                     visuals <- na.omit(pca.conditions.plot.visuals()[selected.conditions,])
-                     
-                     validate(need(nrow(visuals) > 0, "No visual data!"))
-                     
-                     fill <- sapply(rownames(visuals), function(x) { if(visuals[x, "color"] == "") "black" else visuals[x, "color"] })
-                     name <- sapply(rownames(visuals), function(x) { if(visuals[x, "name"] == "") x else visuals[x, "name"] })
-                     
-                     venn.diagram(x, 
-                                  filename, 
-                                  width = width,
-                                  height = height,
-                                  resolution = dpi,
-                                  imagetype = format,
-                                  main = title,
-                                  sub = subtitle,
-                                  fill = as.vector(fill),
-                                  category.names = as.vector(name),
-                                  main.fontfamily = "sans",
-                                  main.fontface = 2,
-                                  main.cex = 1.2,
-                                  sub.fontfamily = "sans",
-                                  sub.cex = 1.1,
-                                  cat.fontfamily = "sans",
-                                  fontfamily = "sans",
-                                  ext.text = T,
-                                  euler.d = T,
-                                  scaled = T)
-                     
-                     return(plot.settings)
-                     
                    })
   
   downloadableDataTable("genes.variance", export.filename = "variance", data = serverGeneVarianceTableData(gene.variances))
