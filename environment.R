e107ecc9f2107d88e9bc0275f4fa3ce52c6ea999
Ruman Gerst <ruman.gerst@uni-jena.de>
2017-12-22 03:05:13 +0100

    * back 2 parallel

--- environment.R
+++ environment.R
@@ -8,7 +8,7 @@ if(!exists("ffmpeg.path")) {
 }
 
 # If enabled, the gene annotation importing runs in a different thread (Disable if you have problems or need debugging)
-parallelized.gene.annotation.importer <- F
+parallelized.gene.annotation.importer <- T
 
 # How many GO terms can be viewed in detail in GO browser?
 go.browser.detailed.maxterms <- 50
