c44d4e5cbf0359da26527b2d12e3cecec66aaeda
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-09-25 12:01:51 +0200

    dependency fix

--- check_r_build_dependencies.sh
+++ check_r_build_dependencies.sh
@@ -6,7 +6,7 @@
 
 echo ">>>>>>> Checking build dependencies ..."
 
-dependencies=(build-essential gfortran libreadline-dev libbz2-dev)
+dependencies=(build-essential gfortran libreadline-dev libbz2-dev liblzma-dev)
 uninstalled_dependencies=()
 debian_check=$(which apt)
 
